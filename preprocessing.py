e34e359b32a43387dae76e954ac8e27243ed91c9
reynawu541 <54989789+reynawu541@users.noreply.github.com>
2023-03-24 14:11:30 -0400

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -96,19 +96,13 @@ def filter(img, msk, patient_path):
   mask_sum = np.sum(msk, axis=(0,1))
   zero_mask = [i for i in range(len(slice_sum)) if mask_sum[i] == 0]
   nonzero_indices = [i for i in range(len(slice_sum)) if slice_sum[i] != 0]
-  # nonzero_img = img[:, :, nonzero_indices]
-  if len(nonzero_indices) >= 128:
-    top_slices = np.argsort(slice_sum)[::-1][:128]
-    top_slices = sorted(top_slices)
-  else:
-    top_slices = nonzero_indices
-  selected_img = img[:, :, top_slices]
-  selected_msk = msk[:,:, top_slices]
+  selected_img = img[:, :, nonzero_indices]
+  selected_msk = msk[:,:, nonzero_indices]
   for i in range(selected_img.shape[2]):
-    temp_img = Image.fromarray(selected_img[:,:,i].astype('uint8')).resize((128,128))
+    temp_img = Image.fromarray(selected_img[:,:,i]).resize((128,128))
     temp_array = np.array(temp_img).reshape(1, 128, 128)
     image_slices.append(temp_array)
-    temp_msk = Image.fromarray(selected_msk[:,:,i].astype('uint8')).resize((128,128))
+    temp_msk = Image.fromarray(selected_msk[:,:,i]).resize((128,128))
     temp_arr_msk = np.array(temp_msk).reshape(1, 128, 128)
     mask_slices.append(temp_arr_msk)
   image_stack = np.stack(image_slices, axis=0)
@@ -148,16 +142,16 @@ if __name__ == "__main__":
         for patient in patient_list:
             patient_path = os.path.join(group_path, patient)
             img, msk = BraTS_Patient_Loader(patient_path)
-            img_torch, msk_torch, zero_mask = filter(img, msk, patient_path)
             msk = BraTS_seg_to_binary(msk)
+            img_torch, msk_torch, zero_mask = filter(img, msk, patient_path)
             print(patient_path)
             masks.append(msk)
             imgs.append(img_torch)
             image_count = img_torch.shape[0]
             end_index = start_index + image_count - 1
             masks_torch.append(msk_torch)
-            start_index = end_index + 1
             indices = list(range(start_index, end_index + 1))
+            start_index = end_index + 1
             patient_indices[patient] = indices
             if group == "LGG":
                 labels.append(0)
@@ -165,6 +159,7 @@ if __name__ == "__main__":
                 labels.append(1)
             pIDs.append(patient)
             file.write(f'{patient}: {zero_mask}\n')
+            file.write(f'{len(zero_mask)}\n')
             if ind%50 == 0:
                 print("working on patient", ind)
             ind += 1
@@ -174,53 +169,12 @@ if __name__ == "__main__":
         print("missing label or image")
     torch.save(img_object, "/content/gdrive/MyDrive/imgs_torch.pt")
     torch.save(mask_object, "/content/gdrive/MyDrive/masks_torch.pt")
-    save_object(imgs, "/content/gdrive/MyDrive/imgs_2516proj.p")
     save_object(labels, "/content/gdrive/MyDrive/label_2516proj.p")
-    save_object(masks, "/content/gdrive/MyDrive/mask_2516proj.p")
 
 """split into train/validation + test (0.8 VS 0.2)"""
 
-root_dir = "/content/gdrive/MyDrive/MICCAI_BraTS_2019_Data_Training"
-groups = ["LGG", "HGG"]
-masks = []
-masks_torch = []
-imgs = []
-labels = []
-pIDs = []
-ind = 1
-patient_indices = {}
-start_index = 0
-for group in groups:
-  group_path = os.path.join(root_dir, group)
-  patient_list = os.listdir(group_path)
-  for patient in patient_list:
-    patient_path = os.path.join(group_path, patient)
-    img, msk = BraTS_Patient_Loader(patient_path)
-    msk = BraTS_seg_to_binary(msk)
-    img_torch, msk_torch, zero_mask = filter(img, msk, patient_path)
-    #msk = BraTS_seg_to_binary(msk)
-    #masks.append(msk)
-    imgs.append(img_torch)
-    image_count = img_torch.shape[0]
-    end_index = start_index + image_count - 1
-    masks_torch.append(msk_torch)
-    indices = list(range(start_index, end_index + 1))
-    patient_indices[patient] = indices
-    start_index = end_index + 1
-
-mask = torch.load("/content/gdrive/MyDrive/MICCAI_BraTS_2019_Data_Training/masks_torch.pt")
-loaded_tensor = torch.load('/content/gdrive/MyDrive/MICCAI_BraTS_2019_Data_Training/imgs_torch.pt')
-
-image_2 = loaded_tensor.float()
-
-# Normalize each image in the tensor to the range [0, 1]
-for i in range(image_2.shape[0]):
-    # Get the ith image from the tensor
-    image_1 = image_2[i, 0, :, :].numpy()/255
-    #normalized_input = (image_1 - np.amin(image_1)) / (np.amax(image_1) - np.amin(image_1))
-  
-    # Update the ith image in the tensor
-    image_2[i, 0, :, :] = torch.from_numpy(image_1)
+img_normalized = img_object/255.
+torch.save(img_normalized, "/content/gdrive/MyDrive/img_normalized.pt")
 
 """268 patients in train (34274 images), 67 patients in test (8576 images), 335 patients in total (42850 images)"""
 
@@ -242,18 +196,18 @@ for patient_id in not_selected_ids:
 indices_tensor = torch.tensor(selected_indices, dtype=torch.long)
 
 # Select the specified indices from the torch object
-train_data = torch.index_select(loaded_tensor, 0, indices_tensor)
+train_data = torch.index_select(img_normalized, 0, indices_tensor)
 
 # Select the specified indices from the torch object
-mask_train = torch.index_select(mask, 0, indices_tensor)
+mask_train = torch.index_select(mask_object, 0, indices_tensor)
 
 test_tensor = torch.tensor(test_indices, dtype=torch.long)
 
 # Select the specified indices from the torch object
-test_data = torch.index_select(loaded_tensor, 0, test_tensor)
+test_data = torch.index_select(img_normalized, 0, test_tensor)
 
 # Select the specified indices from the torch object
-mask_test = torch.index_select(mask, 0, test_tensor)
+mask_test = torch.index_select(mask_object, 0, test_tensor)
 
 torch.save(train_data, '/content/gdrive/MyDrive/MICCAI_BraTS_2019_Data_Training/train_img.pt')
 torch.save(test_data, '/content/gdrive/MyDrive/MICCAI_BraTS_2019_Data_Training/test_img.pt')
