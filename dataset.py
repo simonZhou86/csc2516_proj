d35235e3ab8ff3bf5300838ee4e19fdebce5f58a
simonZhou86 <simon.zhou@queensu.ca>
2023-03-24 14:55:04 -0400

    delete /255.

--- dataset.py
+++ dataset.py
@@ -25,19 +25,19 @@ class BraTS_2d(Dataset):
     
     def load_data(self):
         if self.mode == 'train':
-            self.imgs = torch.load(self.data_dir + 'train_img.pt') / 255.
+            self.imgs = torch.load(self.data_dir + 'train_img.pt')
             self.masks = torch.load(self.data_dir + 'train_mask.pt')
             # spilit train and val
             self.imgs = self.imgs[:self.imgs.shape[0] * 8 // 10]
             self.masks = self.masks[:self.masks.shape[0] * 8 // 10]
         elif self.mode == 'val':
-            self.imgs = torch.load(self.data_dir + 'train_img.pt') / 255.
+            self.imgs = torch.load(self.data_dir + 'train_img.pt')
             self.masks = torch.load(self.data_dir + 'train_mask.pt')
             # spilit train and val
             self.imgs = self.imgs[self.imgs.shape[0] * 8 // 10:]
             self.masks = self.masks[self.masks.shape[0] * 8 // 10:]
         elif self.mode == 'test':
-            self.imgs = torch.load(self.data_dir + 'test_img.pt') / 255.
+            self.imgs = torch.load(self.data_dir + 'test_img.pt')
             self.masks = torch.load(self.data_dir + 'test_mask.pt')
         else:
             raise ValueError('mode should be train or test')
