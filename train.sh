a3aea186b883cfcf4d0bb33d6e67d634df2ef81e
Simon <54770908+simonZhou86@users.noreply.github.com>
2023-04-19 16:31:31 -0400

    run on cc

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes 1
-#SBATCH --gres=gpu:p100:2 # Request GPU "generic resources"
-#SBATCH --cpus-per-task=32 # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --tasks-per-node=2
+#SBATCH --gres=gpu:1 # Request GPU "generic resources"
+#SBATCH --cpus-per-task=4 # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --tasks-per-node=1
 #SBATCH --mem=126000M # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --job-name=train_trail1
 #SBATCH --time=24:00:00
@@ -18,7 +18,7 @@ cd #YOUR PROJECT PATH
 wandb offline
 export MASTER_ADDR=$(hostname) #Store the master node’s IP address in the MASTER_ADDR environment variable.
 
-echo "r$SLURM_NODEID master: $MASTER_ADDR"
-echo "r$SLURM_NODEID Launching python script"
+#echo "r$SLURM_NODEID master: $MASTER_ADDR"
+#echo "r$SLURM_NODEID Launching python script"
 
-srun python main.py --train --viz_wandb csc2516_proj --lr 0.0001 --batch_size 32 --c2 0.2 --lambda1 0.2 --lambda2 0.2 --cross_att --slurm --world_size 2 --init_method tcp://$MASTER_ADDR:3456
+srun python main.py --train --viz_wandb csc2516_proj --lr 0.0001 --batch_size 32 --c2 0.2 --lambda1 0.2 --lambda2 0.2 --cross_att
